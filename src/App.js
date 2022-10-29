497aae4cc840f26cc13b56ab08c86f135a7cdd37
Ushijima Kazuto <46068674+tojimasan@users.noreply.github.com>
2022-10-29 21:01:27 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { Center, Input, VStack } from "@chakra-ui/react";
+import { randomWord } from "./utils/ramdomWord";
+
+export const App = () => {
+  // TODO: ゲームスタート時の処理を追加する
+  // 仕様: 文字が入力されるとゲームが始まること
+  const [problem, setProblem] = useState("Type any button to start.");
+  const [index, setIndex] = useState(0);
+  const [isWrong, setIsWrong] = useState(false);
+  const currectCharCss = {
+    color: "white"
+  };
+  const errorCharCss = {
+    color: "tomato"
+  };
+  const handleChange = (e) => {
+    const pressedKey = e.target.value.slice(-1);
+    // pressedKeyとproblem[index]が同じ文字であれば、次の文字へ進む
+    // TODO: pressedKeyとproblem[index]が同じ文字 && problem.lastOfIndex === indexであれば、次の問題文を提示する
+
+    if (pressedKey === problem[index]) {
+      setIsWrong(false);
+      if (problem.length <= index + 1) {
+        setProblem(randomWord());
+        setIndex(0);
+      } else {
+        setIndex((prev) => prev + 1);
+      }
+    } else {
+      setIsWrong(true);
+    }
+  };
+  const stylingProblemParagraph = (string, i, isError = false) => {
+    const char = string[i];
+    const str1 = string.slice(0, index);
+    const str2 = string.slice(index + 1);
+    const stylyedChar = (
+      <span style={isError ? errorCharCss : currectCharCss}>{char}</span>
+    );
+
+    return (
+      <p>
+        {str1}
+        {stylyedChar}
+        {str2}
+      </p>
+    );
+  };
+
+  return (
+    <Center
+      bg="gray.900"
+      h="100vh"
+      color="gray.500"
+      fontSize="32px"
+      fontWeight="bold"
+    >
+      <VStack>
+        {stylingProblemParagraph(problem, index, isWrong)}
+        <Input opacity="0" autoFocus={true} onChange={(e) => handleChange(e)} />
+      </VStack>
+    </Center>
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { App } from "./App";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
